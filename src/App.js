1e2505a9f0ec2adfbd26792ae87ff779e0d31b21
FenounAchraf <achraffenoun@gmail.com>
2026-02-17 23:36:41 +0000

    Add posts list and create page with JSON server integration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import NewPost from "./pages/NewPost";
+import MyPosts from "./pages/MyPosts";
 
 export default function App() {
   return (
@@ -11,11 +13,28 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
+
         <Route
           path="/"
           element={
             <ProtectedRoute>
               <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/posts/new"
+          element={
+            <ProtectedRoute>
+              <NewPost />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-posts"
+          element={
+            <ProtectedRoute>
+              <MyPosts />
             </ProtectedRoute>
           }
         />
